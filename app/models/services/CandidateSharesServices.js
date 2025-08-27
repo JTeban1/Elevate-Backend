0967168d6522df8f249143f573c2f4e7108d00a0
JuanDavidBarr <barrera.juanda7@gmail.com>
2025-08-27 09:32:46 -0500

    feat(model): add create service, control and route of shares

--- app/models/services/CandidateSharesServices.js
+++ app/models/services/CandidateSharesServices.js
@@ -7,8 +7,8 @@ import Vacancy from '../entities/VacanciesEntity.js';
 export const getCandidateSharesByUserId = async (senderId) => {
     try {
         const candidateShares = await CandidateShares.findAll({
-            where: {sender_id: senderId},
-            include : [
+            where: { sender_id: senderId },
+            include: [
                 {
                     model: Candidate,
                     attributes: ['name', 'email', 'phone', 'occupation'],
@@ -26,6 +26,27 @@ export const getCandidateSharesByUserId = async (senderId) => {
         });
         return candidateShares;
     } catch (error) {
-            console.error('Error fetching candidate shares:', error);
+        console.error('Error fetching candidate shares:', error);
+    }
+};
+
+export const createCandidateShares = async (newShare) => {
+    const {
+        candidate_id,
+        sender_id,
+        receiver_id,
+        application_id,
+    } = newShare
+    try {
+        const newCandidateShare = await CandidateShares.create({
+            candidate_id,
+            sender_id,
+            receiver_id,
+            application_id,
+            status: 'pending',
+        })
+        return newCandidateShare;
+    } catch (error) {
+        console.error('Error creating candidate share:', error);
     }
 };
